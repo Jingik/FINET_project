59fc5a6fc6884f1927465b386a90f34c51e7f24a
jingik <gch05926@gmail.com>
2024-05-19 17:32:18 +0900

    게시판댓글 CRUD

--- Backend/final_pjt_back/boards/serializers.py
+++ Backend/final_pjt_back/boards/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
 from .models import Board, Comment
 
-class BoardSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Board
-        fields = '__all__'
-
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = '__all__'
+        fields = ['id', 'user', 'board', 'parent_comment', 'content', 'created_at', 'updated_at']
+        read_only_fields = ['user', 'board', 'created_at', 'updated_at']
+
+class BoardSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Board
+        fields = ['id', 'user', 'title', 'content', 'created_at', 'updated_at', 'comments']
+        read_only_fields = ['user', 'created_at', 'updated_at']
