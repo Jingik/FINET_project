d7b7fc771c4579041210e030cd2a80c7344f9f45
장유진 <danya6@naver.com>
2024-05-22 00:16:57 +0900

    gointodie

--- Frontend/final_pjt_front/src/views/ExchangeView.vue
+++ Frontend/final_pjt_front/src/views/ExchangeView.vue
@@ -3,30 +3,55 @@
     <h1 class="title">국가별 환전 계산기</h1>
     <ul class="cny">
       <li>
-        '최종고시환율'은 오늘 기준 가장 최근 고시된 환율정보를 나타내며,
-        은행의 환율고시가 종료되지 않은 상태일 수 있습니다.
+        '최종고시환율'은 오늘 기준 가장 최근 고시된 환율정보를 나타내며, 은행의
+        환율고시가 종료되지 않은 상태일 수 있습니다.
       </li>
       <li>
-        중국 위안화(CNY)는 홍콩 외환시장에서 거래되는 위안화
-        환율정보(CNH)를 고시합니다.
+        중국 위안화(CNY)는 홍콩 외환시장에서 거래되는 위안화 환율정보(CNH)를
+        고시합니다.
       </li>
       <li>일본은 100엔을 기준으로 합니다.</li>
     </ul>
     <div class="button-container">
-<div class="currency-buttons">
-  <div v-for="(currency, index) in currencies" :key="currency">
-    <img 
-      @click="fetchExchangeRate(currency, currencyUnits[currency])"
-      :src="'/src/assets/img/' + currency + '.png'" 
-      alt="Currency Image" 
-      style="cursor: pointer; margin-right: 20px;">
-  </div>
-</div>
-
-
+      <div class="currency-buttons">
+        <div v-for="(currency, index) in currencies" :key="currency">
+          <img
+            @click="fetchExchangeRate(currency, currencyUnits[currency])"
+            :src="'/src/assets/img/' + currency + '.png'"
+            alt="Currency Image"
+            style="cursor: pointer; margin-right: 20px"
+          />
+        </div>
+      </div>
     </div>
+
     <div v-if="selectedExchangeRate" class="exchange-rate-details">
-      <h2 class="currency-title">{{ selectedExchangeRate.cur_unit }}</h2>
+      <div class="helpme">
+      <h1>{{ selectedExchangeRate.cur_unit }}</h1>
+      <!-- 외화 살 때, 외화 팔 때 버튼 -->
+      <div class="case-button-container">
+        <button
+          @click="showDiv(1)"
+          :class="
+            selectedDiv === 1
+              ? 'case-button'
+              : 'case-button'
+          "
+        >
+          외화 살 때
+        </button>
+        <button
+          @click="showDiv(2)"
+          :class="
+            selectedDiv === 2
+              ? 'case-button'
+              : 'case-button'
+          "
+        >
+          외화 팔 때
+        </button>
+      </div>
+    </div>
       <div class="exchange-rate-row">
         <div class="exchange-rate-item">
           <span class="label">송금 받을 때</span>
@@ -41,25 +66,72 @@
           <span class="value">{{ selectedExchangeRate.deal_bas_r }}</span>
         </div>
       </div>
-      <div class="calculation-container">
+
+      <div v-show="selectedDiv === 1" class="calculation-container" id="1">
         <div class="input-container">
-          <label for="amount" class="input-label">사시는 금액(외화)</label>
+          <label for="amountKRW" class="input-label"
+            >지불해야 할 금액(원화)</label
+          >
           <div class="input-box-with-unit">
-            <input type="number" v-model="wanttobuy" id="amount" class="input-box" />
-            <p class="currency-unit">{{ selectedCurrencyUnit }}</p>
+            <input
+              type="number"
+              v-model="wanttobuyKRW"
+              id="amountKRW"
+              class="input-box"
+            />
+            <p class="currency-unit">원</p>
           </div>
         </div>
         <div class="input-container">
-          <label for="calculationMethod" class="input-label">계산방식</label>
-          <select v-model="calculationMethod" id="calculationMethod" class="input-box">
+          <label for="calculationMethodKRW" class="input-label"
+            >계산 방식</label
+          >
+          <select
+            v-model="calculationMethodKRW"
+            id="calculationMethodKRW"
+            class="input-box"
+          >
             <option value="deal_bas_r">매매기준율</option>
             <option value="ttb">송금 받을 때</option>
             <option value="tts">송금 보낼 때</option>
           </select>
         </div>
         <div class="result-container">
-          <span class="label">결제금액(원화)</span>
-          <span class="value">{{ formattedResult }}원</span>
+          <span class="label">살 수 있는 금액(외화)</span>
+          <span class="value"
+            >{{ formattedResultKRW }} {{ selectedCurrencyUnit }}</span
+          >
+        </div>
+      </div>
+
+      <div v-show="selectedDiv === 2" class="calculation-container" id="2">
+        <div class="input-container">
+          <label for="amount" class="input-label">갖고 있는 금액(외화)</label>
+          <div class="input-box-with-unit">
+            <input
+              type="number"
+              v-model="wanttobuy"
+              id="amount"
+              class="input-box"
+            />
+            <p class="currency-unit">{{ selectedCurrencyUnit }}</p>
+          </div>
+        </div>
+        <div class="input-container">
+          <label for="calculationMethod" class="input-label">계산 방식</label>
+          <select
+            v-model="calculationMethod"
+            id="calculationMethod"
+            class="input-box"
+          >
+            <option value="deal_bas_r">매매기준율</option>
+            <option value="ttb">송금 받을 때</option>
+            <option value="tts">송금 보낼 때</option>
+          </select>
+        </div>
+        <div class="result-container">
+          <span class="label">받으실 금액(원화)</span>
+          <span class="value">{{ formattedResult }} </span>
         </div>
       </div>
     </div>
@@ -73,38 +145,62 @@
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';
-import axios from 'axios';
+import { ref, computed } from "vue";
+import axios from "axios";
 
 const currencies = [
-  'USD', 'JPY(100)', 'EUR', 'GBP', 'CAD', 'CHF', 'HKD', 'AUD', 'CNH', 'SGD', 'NZD', 'THB'
+  "USD",
+  "JPY(100)",
+  "EUR",
+  "GBP",
+  "CAD",
+  "CHF",
+  "HKD",
+  "AUD",
+  "CNH",
+  "SGD",
+  "NZD",
+  "THB",
 ];
 
 const currencyname = [
-  '미국달러', '일본엔', '유럽유로', '영국파운드', '캐나다달러', '스위스프랑', '홍콩달러', '호주달러', '중국위안', '싱가폴달러', '뉴질랜드달러', '태국바트'
+  "미국달러",
+  "일본엔",
+  "유럽유로",
+  "영국파운드",
+  "캐나다달러",
+  "스위스프랑",
+  "홍콩달러",
+  "호주달러",
+  "중국위안",
+  "싱가폴달러",
+  "뉴질랜드달러",
+  "태국바트",
 ];
 
 const currencyUnits = {
-  'USD': '달러',
-  'JPY(100)': '백엔',
-  'EUR': '유로',
-  'GBP': '파운드',
-  'CAD': '캐나다 달러',
-  'CHF': '스위스 프랑',
-  'HKD': '홍콩 달러',
-  'AUD': '호주 달러',
-  'CNH': '위안',
-  'SGD': '싱가포르 달러',
-  'NZD': '뉴질랜드 달러',
-  'THB': '바트'
+  USD: "달러",
+  "JPY(100)": "백엔",
+  EUR: "유로",
+  GBP: "파운드",
+  CAD: "캐나다 달러",
+  CHF: "스위스 프랑",
+  HKD: "홍콩 달러",
+  AUD: "호주 달러",
+  CNH: "위안",
+  SGD: "싱가포르 달러",
+  NZD: "뉴질랜드 달러",
+  THB: "바트",
 };
 
 const selectedExchangeRate = ref(null);
-const selectedCurrencyUnit = ref('');
+const selectedCurrencyUnit = ref("");
 const error = ref(null);
 const loading = ref(false);
 const wanttobuy = ref(null); // null로 초기화
-const calculationMethod = ref('deal_bas_r');
+const wanttobuyKRW = ref(null); // 원화 입력값
+const calculationMethod = ref("deal_bas_r");
+const calculationMethodKRW = ref("deal_bas_r");
 
 const fetchExchangeRate = async (curUnit, curUnitName) => {
   loading.value = true;
@@ -113,10 +209,12 @@ const fetchExchangeRate = async (curUnit, curUnitName) => {
   selectedCurrencyUnit.value = curUnitName;
 
   try {
-    const response = await axios.get(`http://127.0.0.1:8000/exchange/${curUnit}/`);
+    const response = await axios.get(
+      `http://127.0.0.1:8000/exchange/${curUnit}/`
+    );
     selectedExchangeRate.value = response.data;
   } catch (err) {
-    error.value = '데이터를 가져오는 중 오류가 발생했습니다.';
+    error.value = "데이터를 가져오는 중 오류가 발생했습니다.";
     console.error(err);
   } finally {
     loading.value = false;
@@ -125,22 +223,53 @@ const fetchExchangeRate = async (curUnit, curUnitName) => {
 
 const calculateResult = () => {
   if (!selectedExchangeRate.value || !wanttobuy.value) return 0; // 값이 null이면 계산하지 않음
-  const rate = Number(selectedExchangeRate.value[calculationMethod.value].replace(/,/g, '')); // 콤마 제거 후 숫자로 변환
+  const rate = Number(
+    selectedExchangeRate.value[calculationMethod.value].replace(/,/g, "")
+  ); // 콤마 제거 후 숫자로 변환
   const amount = parseFloat(wanttobuy.value); // 입력된 값이 숫자인지 확인
-  if (isNaN(amount)) { // 숫자가 아닌 경우
-    error.value = '올바른 숫자를 입력하세요.'; // 오류 메시지 표시
+  if (isNaN(amount)) {
+    // 숫자가 아닌 경우
+    error.value = "올바른 숫자를 입력하세요."; // 오류 메시지 표시
     return 0;
   }
   return rate * amount;
 };
 
+const calculateResultKRW = () => {
+  if (!selectedExchangeRate.value || !wanttobuyKRW.value) return 0; // 값이 null이면 계산하지 않음
+  const rate = Number(
+    selectedExchangeRate.value[calculationMethodKRW.value].replace(/,/g, "")
+  ); // 콤마 제거 후 숫자로 변환
+  const amount = parseFloat(wanttobuyKRW.value); // 입력된 값이 숫자인지 확인
+  if (isNaN(amount)) {
+    // 숫자가 아닌 경우
+    error.value = "올바른 숫자를 입력하세요."; // 오류 메시지 표시
+    return 0;
+  }
+  return amount / rate; // 원화에서 외화로 변환
+};
+
 const formattedResult = computed(() => {
   const result = calculateResult();
+  return new Intl.NumberFormat().format(result) + "원";
+});
+
+const formattedResultKRW = computed(() => {
+  const result = calculateResultKRW();
   return new Intl.NumberFormat().format(result);
 });
-</script>
 
+const selectedDiv = ref(null);
+
+const showDiv = (divId) => {
+  selectedDiv.value = divId;
+};
+</script>
 <style scoped>
+.helpme {
+  display:flex;
+  margin-bottom: 30px;
+}
 .exchange-rates-container {
   max-width: 1800px;
   margin: 100px auto;
@@ -162,17 +291,39 @@ const formattedResult = computed(() => {
   text-align: center;
 }
 
+.select-button-container {
+  display: block;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width:1200px;
+  margin-bottom: 50px;
+  margin: auto;
+}
 .button-container {
   display: block;
   flex-direction: row;
   align-items: center;
   justify-content: center;
-  height: 100px;
   width:1200px;
   margin-bottom: 50px;
   margin: auto;
 }
 
+.case-button {
+    margin-left: 10px;
+    background-color: #fff; /* 기본 색상은 하얀색 */
+    color: #000; /* 텍스트 색상은 검은색 */
+    border: 1px solid #000; /* 테두리는 검은색 */
+    transition: background-color 0.3s, color 0.3s; /* 전환 효과 */
+    border-radius: 10px;
+  }
+
+  .case-button:hover {
+    background-color: #0056b3; /* 마우스를 올렸을 때의 배경색 */
+    color: #fff; /* 마우스를 올렸을 때의 텍스트 색상 */
+  }
+  
 .currency-buttons {
   display: flex;
   flex-wrap: wrap;
