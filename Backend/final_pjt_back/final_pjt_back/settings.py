2489e4d72bd098c59d13d340bae8f542aa55c7e9
Psunmin <bbu0330@gmail.com>
2024-05-23 11:50:23 +0900

    메인페이지 수정

--- Backend/final_pjt_back/final_pjt_back/settings.py
+++ Backend/final_pjt_back/final_pjt_back/settings.py
@@ -137,23 +137,23 @@ CORS_ALLOWED_ORIGINS = [
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'finet',
-        'USER': 'yujin',
-        'PASSWORD': '1234',
-        'HOST': 'localhost', # MySQL 호스트
-        'PORT': '3306',      # MySQL 포트 (기본값은 3306)
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'finet',
+#         'USER': 'yujin',
+#         'PASSWORD': '1234',
+#         'HOST': 'localhost', # MySQL 호스트
+#         'PORT': '3306',      # MySQL 포트 (기본값은 3306)
+#     }
+# }
 
 
 
